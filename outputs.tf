19d6268d99bd0c8df481c41a2dd596ad1aa0c13c
Alexander Sharov <kvendingoldo@yandex.ru>
2024-11-18 01:55:56 +0400

    feat: add version_id to output

--- outputs.tf
+++ outputs.tf
@@ -7,3 +7,8 @@ output "name" {
   description = "Lockbox secret name"
   value       = yandex_lockbox_secret.main.name
 }
+
+output "version_id" {
+  description = "Lockbox version id"
+  value       = yandex_lockbox_secret_version.main.id
+}
